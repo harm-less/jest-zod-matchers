2843c29d7501bd28026530f6d8151567270ce93e
Harm van der Werf <harmvdwerf@gmail.com>
2025-03-06 16:09:28 +0100

    fix: matcher types as they may be nullable

--- src/matchers.ts
+++ src/matchers.ts
@@ -4,11 +4,8 @@ import {toThrowZodError} from './matchers/to-throw-zod-error.matcher';
 declare global {
 	namespace jest {
 		interface Matchers<R> {
-			toBeValidZodObject<Response extends Record<string, unknown>>(
-				zodOutputObject?: Partial<{
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-					[K in keyof Response]: any;
-				}>
+			toBeValidZodObject<Response extends Record<string, unknown> | null | undefined>(
+				zodOutputObject?: Partial<Response>
 			): R;
 			toThrowZodError(expectedError: string): R;
 		}
